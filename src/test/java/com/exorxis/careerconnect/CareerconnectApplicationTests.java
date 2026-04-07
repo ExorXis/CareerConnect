623fa81b1b57d5cbaa69b77185940826be9b8056
ExorXis <alokverma1.rtk@gmail.com>
2026-04-07 21:56:01 +0530

    Day 1 - Success: Basic User Controller running and ping successful

---
+++ src/test/java/com/exorxis/careerconnect/CareerconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exorxis.careerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
